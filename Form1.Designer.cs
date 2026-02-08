db9e2a804921fc00fc8919562b82759b53c0d956
Yostina Romani <fgyhfugh@gmail.com>
2026-02-08 11:04:50 +0200

    First commit - X O Game WinForms

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace X_O_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "o.jpg");
+            this.imageList1.Images.SetKeyName(1, "x.jpg");
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(118, 31);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(555, 319);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox7);
+            this.tabPage1.Controls.Add(this.pictureBox8);
+            this.tabPage1.Controls.Add(this.pictureBox9);
+            this.tabPage1.Controls.Add(this.pictureBox4);
+            this.tabPage1.Controls.Add(this.pictureBox5);
+            this.tabPage1.Controls.Add(this.pictureBox6);
+            this.tabPage1.Controls.Add(this.pictureBox3);
+            this.tabPage1.Controls.Add(this.pictureBox2);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(547, 293);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tic Tac Toe";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox7.Location = new System.Drawing.Point(375, 191);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(129, 74);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox8.Location = new System.Drawing.Point(375, 100);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(129, 74);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox9.Location = new System.Drawing.Point(375, 6);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(129, 74);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox9.TabIndex = 6;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox4.Location = new System.Drawing.Point(209, 191);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(129, 74);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox5.Location = new System.Drawing.Point(209, 100);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(129, 74);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox6.Location = new System.Drawing.Point(209, 6);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(129, 74);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(47, 191);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(129, 74);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(47, 100);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(129, 74);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(47, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::X_O_Game.Properties.Resources.images;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,519 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_O_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void disable()
+        {
+            pictureBox1.Enabled = false;
+            pictureBox2.Enabled = false;
+            pictureBox3.Enabled = false;
+            pictureBox4.Enabled = false;
+            pictureBox5.Enabled = false;
+            pictureBox6.Enabled = false;
+            pictureBox7.Enabled = false;
+            pictureBox8.Enabled = false;
+            pictureBox9.Enabled = false;
+
+            tabControl1.Visible = false;
+
+        }
+        int i = 0, a = 1, r = 2, d = 3, c = 4, f = 5, h = 6, l = 7, m = 8, n = 9,q;
+        bool b = true;
+        int ran()
+        {
+            Random rand = new Random();
+            int x = rand.Next(10);
+            return x;
+        }
+        void chang(int q)
+        {
+            if (q == 1)
+            {
+                if (!b)
+                {
+                    i = 1;
+                    a = 1;
+                    b = true;
+                    pictureBox1.Image = imageList1.Images[i];
+
+                }
+                else
+                {
+                    b = false;
+                    i = 0;
+                    pictureBox1.Image = imageList1.Images[i];
+                    a = 0;
+                }
+                if ((a == m && a == l) || (a == r && a == d) || (a == n && a == f))
+                {
+                    disable();
+
+                    MessageBox.Show("you won");
+                }
+            }
+            else if (q == 2)
+            {
+                if (!b)
+                {
+                    i = 1;
+                    a = 1;
+                    b = true;
+                    pictureBox2.Image = imageList1.Images[i];
+
+                }
+                else
+                {
+                    b = false;
+                    i = 0;
+                    pictureBox2.Image = imageList1.Images[i];
+                    a = 0;
+                }
+                if ((r == h && r == n) || (a == r && a == d))
+                {
+                    disable();
+
+                    MessageBox.Show("you won");
+                }
+
+            }
+            else if (q == 3)
+            {
+                if (!b)
+                {
+                    i = 1;
+                    a = 1;
+                    b = true;
+                    pictureBox3.Image = imageList1.Images[i];
+
+                }
+                else
+                {
+                    b = false;
+                    i = 0;
+                    pictureBox3.Image = imageList1.Images[i];
+                    a = 0;
+                }
+                if ((d == c && d == f) || (a == r && a == d) || (d == n && d == l))
+                {
+                    disable();
+
+                    MessageBox.Show("you won");
+                }
+
+            }
+            else if (q == 5)
+            {
+                if (!b)
+                {
+                    i = 1;
+                    a = 1;
+                    b = true;
+                    pictureBox5.Image = imageList1.Images[i];
+
+                }
+                else
+                {
+                    b = false;
+                    i = 0;
+                    pictureBox5.Image = imageList1.Images[i];
+                    a = 0;
+                }
+                if ((r == h && r == n) || (f == h && f == l))
+                {
+                    disable();
+
+                    MessageBox.Show("you won");
+                }
+            }
+            else if (q == 4)
+            {
+                if (!b)
+                {
+                    i = 1;
+                    a = 1;
+                    b = true;
+                    pictureBox4.Image = imageList1.Images[i];
+
+                }
+                else
+                {
+                    b = false;
+                    i = 0;
+                    pictureBox4.Image = imageList1.Images[i];
+                    a = 0;
+                }
+                if ((d == c && d == f) || (c == n && c == m))
+                {
+                    disable();
+
+                    MessageBox.Show("you won");
+                }
+            }
+            else if (q == 6)
+            {
+                if (!b)
+                {
+                    i = 1;
+                    a = 1;
+                    b = true;
+                    pictureBox6.Image = imageList1.Images[i];
+
+                }
+                else
+                {
+                    b = false;
+                    i = 0;
+                    pictureBox6.Image = imageList1.Images[i];
+                    a = 0;
+                }
+                if ((a == m && a == l) || (m == n && m == c))
+                {
+                    disable();
+
+                    MessageBox.Show("you won");
+                }
+            }
+            else if (q == 7)
+            {
+                if (!b)
+                {
+                    i = 1;
+                    a = 1;
+                    b = true;
+                    pictureBox7.Image = imageList1.Images[i];
+
+                }
+                else
+                {
+                    b = false;
+                    i = 0;
+                    pictureBox7.Image = imageList1.Images[i];
+                    a = 0;
+                }
+                if ((f == h && f == l) || (f == c && f == d) || (f == n && f == a))
+                {
+                    disable();
+
+                    MessageBox.Show("you won");
+                }
+            }
+            else if (q == 9)
+            {
+                if (!b)
+                {
+                    i = 1;
+                    a = 1;
+                    b = true;
+                    pictureBox9.Image = imageList1.Images[i];
+
+                }
+                else
+                {
+                    b = false;
+                    i = 0;
+                    pictureBox9.Image = imageList1.Images[i];
+                    a = 0;
+                }
+                if ((a == m && a == l) || (l == n && n == d) || (l == h && l == f))
+                {
+                    disable();
+
+                    MessageBox.Show("you won");
+                }
+            }
+            else if (q == 8)
+            {
+                if (!b)
+                {
+                    i = 1;
+                    b = true;
+                    pictureBox8.Image = imageList1.Images[i];
+                    h = 1;
+
+                }
+                else
+                {
+                    b = false;
+                    i = 0;
+                    h = 0;
+                    pictureBox8.Image = imageList1.Images[i];
+                }
+                if ((r == h && r == n) || (f == h && f == l))
+                {
+                    disable();
+
+                    MessageBox.Show("you won");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = 10000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            q = ran();
+            chang(q);
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (!b)
+            {
+                i = 1;
+                a = 1;
+                b = true;
+                pictureBox1.Image = imageList1.Images[i];
+
+            }
+            else
+            {
+                b = false;
+                i = 0;
+                pictureBox1.Image = imageList1.Images[i];
+                a = 0;
+            }
+            if ((a == m && a == l) || (a == r && a == d) || (a == n && a == f))
+            {
+                disable();
+
+                MessageBox.Show("you won");
+            }
+
+        } 
+    
+        private void pictureBox6_Click(object sender, EventArgs e)
+    {
+
+        if (!b)
+        {
+            i = 1;
+            b = true;
+            pictureBox6.Image = imageList1.Images[i];
+            m = 1;
+
+        }
+        else
+        {
+            b = false;
+            i = 0;
+            pictureBox6.Image = imageList1.Images[i];
+            m = 0;
+        }
+        if ((a == m && a == l) || (m == n && m == c))
+        {
+            disable();
+
+            MessageBox.Show("you won");
+        }
+    } 
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+
+            if (!b)
+            {
+                i = 1;
+                b = true;
+                pictureBox9.Image = imageList1.Images[i];
+                l = 1;
+
+            }
+            else
+            {
+                b = false;
+                i = 0;
+                pictureBox9.Image = imageList1.Images[i];
+                l = 0;
+            }
+            if ((a == m && a == l) || (l == n && n == d)||(l==h&&l==f))
+            {
+                disable();
+
+                MessageBox.Show("you won");
+            }
+        }
+
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+
+            if (!b)
+            {
+                i = 1;
+                b = true;
+                pictureBox2.Image = imageList1.Images[i];
+                r = 1;
+                
+
+            }
+            else
+            {
+                b = false;
+                i = 0;
+                pictureBox2.Image = imageList1.Images[i];
+                r = 0;
+                
+            }
+            if ((r== h&& r == n) || (a == r && a == d))
+            {
+                disable();
+
+                MessageBox.Show("you won");
+            }
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+            if (!b)
+            {
+                i = 1;
+                b = true;
+                pictureBox5.Image = imageList1.Images[i];
+                n = 1;
+
+            }
+            else
+            {
+                b = false;
+                i = 0;
+                pictureBox5.Image = imageList1.Images[i];
+                n = 0;
+            }
+            if ((n== m && n == c) || (n== r && n == h)||(a==n&&a==f))
+            {
+                disable();
+
+                MessageBox.Show("you won");
+            }
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+            if (!b)
+            {
+                i = 1;
+                b = true;
+                pictureBox8.Image = imageList1.Images[i];
+                h = 1;
+
+            }
+            else
+            {
+                b = false;
+                i = 0;
+                h = 0;
+                pictureBox8.Image = imageList1.Images[i];
+            }
+            if ((r == h && r== n) || (f == h && f == l))
+            {
+                disable();
+
+                MessageBox.Show("you won");
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+            if (!b)
+            {
+                i = 1;
+                b = true;
+                pictureBox3.Image = imageList1.Images[i];
+                d = 1;
+
+            }
+            else
+            {
+                b = false;
+                i = 0;
+                pictureBox3.Image = imageList1.Images[i];
+                d = 0;
+                
+            }
+            if ((d == c && d == f) || (a == r && a == d)||(d==n&&d==l))
+            {
+                disable();
+
+                MessageBox.Show("you won");
+            }
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+            if (!b)
+            {
+                i = 1;
+                b = true;
+                pictureBox4.Image = imageList1.Images[i];
+                c = 1;
+
+            }
+            else
+            {
+                b = false;
+                i = 0;
+                pictureBox4.Image = imageList1.Images[i];
+                c = 0;
+            }
+            if ((d== c && d == f) || (c == n && c == m))
+            {
+                disable();
+
+                MessageBox.Show("you won");
+            }
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+            if (!b)
+            {
+                i = 1;
+                b = true;
+                pictureBox7.Image = imageList1.Images[i];
+                f = 1;
+            }
+            else
+            {
+                b = false;
+                i = 0;
+                pictureBox7.Image = imageList1.Images[i];
+                f = 0;
+            }
+            if ((f == h && f == l) || (f== c && f == d)||(f==n&&f==a))
+            {
+                disable();
+
+                MessageBox.Show("you won");
+            }
+        }
+    }
+}
